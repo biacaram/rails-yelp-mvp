1076cecd860f5040c5460665d5009432b98493dd
beatriz andre <biacaram@hotmail.com>
2020-11-18 19:03:13 -0200

    Yelp com pouco css

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -20,7 +20,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:rating, :content)
+    params.require(:review).permit(:content, :rating)
   end
 
 end
